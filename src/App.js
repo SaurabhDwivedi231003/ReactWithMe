13d9adc775c219daad616278f93ea349269b57a7
SAURABH <saurabhdwivedi2310@gmail.com>
2023-12-18 16:14:49 +0530

    import export along with diffrent ways of wrtiting props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {Video } from "./component/Video"
+
 function App(){
   return (
     <div>
-      Hello Motto
+      <Video title = "React JS Understanding" combo={{ img : "https://placebear.com/160/90" , bgColor : "black"}}/>
+      <Video title="Props Understanding" combo={{ img : "https://placebear.com/160/90" , bgColor : "Red"}}/>
     </div>
   )
 }
--- src/component/Video.js
+++ src/component/Video.js
@@ -1,10 +1,25 @@
-function Video() {
+import './Video.css'
+
+function Video({title , combo}) {   //We would have written props and used it by props.title , props.img but its not a good way
+    let bg = 'ReactTut'
+    console.log(title);
     return(
-<div>
-    Hello Im inside Video
-</div>
-    )
+        <> 
 
-}
+       <div className='pic'>
+        <img src={combo.img} alt="Katherine Johnson" />
+        </div>
+        <div className={bg} style={{backgroundColor : combo.bgColor , margin : "10px 0px",width : '20rem'}}>
+            {title}
+       </div>
 
-export default Video;
+
+        </>
+        )
+    }
+
+export {Video};
+
+// combo k andar do cheezen thi issiliye combo.img , combo.bgColor
+// ye sab alg alg tareeke hain choose anyone
+// https://react.dev/learn/passing-props-to-a-component
